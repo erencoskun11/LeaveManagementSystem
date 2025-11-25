b1effe573c1c698b13342423b2203c6e28ddd434
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-11-25 12:32:23 +0300

    feat: add leave request Service interface

---
+++ Application/Common/Interfaces/Services/ILeaveRequestService.cs
@@ -0,0 +1,17 @@
+﻿using Application.DTOs.LeaveRequest;
+using Domain.Entities; // Eğer Entity kullanıyorsan
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+// HATA BURADAYDI: Muhtemelen sende "Application.Services" yazıyor.
+// AŞAĞIDAKİ GİBİ OLMALI:
+namespace Application.Common.Interfaces.Services
+{
+    public interface ILeaveRequestService
+    {
+        Task<int> CreateLeaveRequestAsync(CreateLeaveRequestDto request, string userEmail);
+        Task<List<LeaveRequestDetailDto>> GetMyLeaveRequestsAsync(string userEmail);
+        Task<List<LeaveRequestDetailDto>> GetAllLeaveRequestsAsync();
+        Task<bool> ManageLeaveRequestAsync(int requestId, bool approved, string managerEmail);
+    }
+}
