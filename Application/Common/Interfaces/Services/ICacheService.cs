37e759e62753a425ff926ba3f73107851de3b031
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-11-25 17:12:26 +0300

    feat (sevice):  created

---
+++ Application/Common/Interfaces/Services/ICacheService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common.Interfaces.Services
+{
+    public interface ICacheService
+    {
+        T Get<T>(string key);
+
+        void Set<T>(string key,T value,TimeSpan? expiration = null);
+
+        void Remove(string key);
+        bool TryGetValue<T>(string key, out T value);
+
+
+
+
+    }
+}
