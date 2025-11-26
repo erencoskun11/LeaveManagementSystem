db3031c8f720cf169da6ca4d7ba704d9388367a4
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-11-26 20:48:23 +0300

    created

---
+++ Application/DTOs/Auth/RegisterRequestDto.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Auth
+{
+    public class RegisterRequestDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int AnnualLeaveAllowance { get; set; }
+        public Role Role { get; set; } // Employee, HR
+        public int DepartmentId { get; set; }
+        public DateTime HireDate { get; set; }
+    }
+}
