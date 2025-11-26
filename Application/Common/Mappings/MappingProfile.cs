9c156b45ded12b4715b4aef88e6d61e034a871f9
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-11-26 20:44:38 +0300

    feat(mapping) : created

---
+++ Application/Common/Mappings/MappingProfile.cs
@@ -0,0 +1,26 @@
+﻿using Application.DTOs.LeaveRequest;
+using AutoMapper;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // LeaveRequest -> LeaveRequestDetailDto Dönüşümü
+            CreateMap<LeaveRequest, LeaveRequestDetailDto>()
+                .ForMember(dest => dest.EmployeeName, opt => opt.MapFrom(src => src.Employee.FirstName + " " + src.Employee.LastName))
+                .ForMember(dest => dest.LeaveTypeName, opt => opt.MapFrom(src => src.LeaveType.Name))
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString()));
+
+            // CreateLeaveRequestDto -> LeaveRequest Dönüşümü (Tersine işlem)
+            CreateMap<CreateLeaveRequestDto, LeaveRequest>();
+        }
+    }
+}
