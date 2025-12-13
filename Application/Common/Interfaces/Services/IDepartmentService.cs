da6c150fce635d4bceea66f10166a99443e28f8f
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-12-13 22:51:54 +0300

    feat(IDepartment) : updated

---
+++ Application/Common/Interfaces/Services/IDepartmentService.cs
@@ -0,0 +1,22 @@
+﻿using Application.DTOs.Department;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common.Interfaces.Services
+{
+    public interface IDepartmentService
+    {
+        Task<List<DepartmentDto>> GetAllDepartmentsAsync();
+
+        Task<DepartmentDto> GetDepartmentByIdAsync(int departmentId);
+        Task CreateDepartmentAsync(CreateDepartmentDto departmentDto);
+
+        Task UpdateDepartmentAsync(UpdateDepartmentDto departmentDto);
+
+        Task DeleteDepartmentAsync(int departmentId);
+
+    }
+}
