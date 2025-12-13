7ae7a69730dabaf8f74fb8fad131bb3fa21b7fc8
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-12-13 22:47:54 +0300

    feat(extensions) : created

---
+++ Application/Common/Extensions/DateExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Application.Common.Extensions
+{
+    public static class DateExtensions
+    {
+        public static int CalculateBusinessDays(this DateTime startDate, DateTime endDate)
+        {
+            int businessDays = 0;
+            for (DateTime date = startDate; date <= endDate; date = date.AddDays(1))
+            {
+                if (date.DayOfWeek != DayOfWeek.Saturday && date.DayOfWeek != DayOfWeek.Sunday)
+                {
+                    businessDays++;
+                }
+            }
+            return businessDays;
+        }
+    }
+}
