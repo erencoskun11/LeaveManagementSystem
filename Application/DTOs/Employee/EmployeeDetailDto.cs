82abd5645634fe57d6102b7cdffc339d9227ef17
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-12-13 22:57:53 +0300

    feat(dto) :  created

---
+++ Application/DTOs/Employee/EmployeeDetailDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Application.DTOs.Employee
+{
+    public class EmployeeDetailDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int Role { get; set; }
+        public int AnnualLeaveAllowance { get; set; }
+        public int DepartmentId { get; set; }
+        public DateTime HireDate { get; set; }
+    }
+}
