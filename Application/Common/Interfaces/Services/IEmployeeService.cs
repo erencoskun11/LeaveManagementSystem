1c4b9156bfe6743318284c7d71d5b6b287b10539
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-12-13 22:53:41 +0300

    feat(IEmployee): created

---
+++ Application/Common/Interfaces/Services/IEmployeeService.cs
@@ -0,0 +1,18 @@
+﻿using Application.DTOs.Employee; 
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Application.Common.Interfaces.Services
+{
+    public interface IEmployeeService
+    {
+        Task<List<EmployeeListDto>> GetAllEmployeesAsync();
+
+        Task<EmployeeDetailDto> GetEmployeeByIdAsync(int id);
+
+        Task UpdateEmployeeAsync(UpdateEmployeeDto request);
+
+        // Silme işlemi (Soft Delete)
+        Task DeleteEmployeeAsync(int id);
+    }
+}
