43cdb0f804bf231b3d24622b31b2755983512256
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2026-02-13 17:30:31 +0300

    feat(repository) : created for every repositories

---
+++ Application/Common/Interfaces/Repositories/IGenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Common; // BaseEntity'nin olduğu namespace (Bunu eklemeyi unutma!)
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System;
+
+namespace Application.Common.Interfaces.Repositories
+{
+    // DÜZELTME: 'where T : class' YERİNE 'where T : BaseEntity' YAZIYORUZ
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+
+        // Best Practice Metotları
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null, bool trackChanges = true);
+        Task<T> GetAsync(Expression<Func<T, bool>> filter, bool trackChanges = true);
+        Task<bool> ExistsAsync(Expression<Func<T, bool>> filter);
+    }
+}
