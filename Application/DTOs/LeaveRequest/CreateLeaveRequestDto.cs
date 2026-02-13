f8a7451d0c8793bab9fe75b33dd48b2fc23c1d22
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2026-02-13 17:37:45 +0300

    feat(dto) : created

--- Application/DTOs/LeaveRequest/CreateLeaveRequestDto.cs
+++ Application/DTOs/LeaveRequest/CreateLeaveRequestDto.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace Application.DTOs.LeaveRequest
 {
-    internal class CreateLeaveRequestDto
+    public class CreateLeaveRequestDto
     {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int LeaveTypeId { get; set; }
+        public string? RequestComments { get; set; }
+
+        // YENİ EKLENEN: Admin başkası için izin girebilsin diye
+        public string? EmployeeId { get; set; } // String veya int (Senin Employee tablon int ise int yap)
     }
 }
