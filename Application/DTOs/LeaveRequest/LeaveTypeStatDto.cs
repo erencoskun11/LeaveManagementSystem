f00b6e8f11ceda6dd269b5c7123d7b882f2ca6ab
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2026-02-13 17:40:35 +0300

    feat(dto) : created

---
+++ Application/DTOs/LeaveRequest/LeaveTypeStatDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.LeaveRequest
+{
+    public class LeaveTypeStatDto
+    {
+        public string LeaveTypeName { get; set; }
+        public int DaysUsed { get; set; }
+
+        public List<LeaveTypeStatDto> LeaveStats { get; set; } = new List<LeaveTypeStatDto>();
+
+        public int TotalDays { get; set; }
+
+
+
+    }
+}
