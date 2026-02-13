055eaf1027259b4361b427aa54c98c1bcbd4cd92
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2026-02-13 17:38:26 +0300

    feat(dto): created

---
+++ Application/DTOs/LeaveRequest/LeaveRequestDetailDto.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.LeaveRequest
+{
+    public class LeaveRequestDetailDto
+    {
+        public int Id { get; set; }
+        public int EmployeeId { get; set; } 
+        public string EmployeeName { get; set; }
+        public string LeaveTypeName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Status { get; set; }
+        public DateTime RequestDate { get; set; }
+        public string RequestComments { get; set; } // Açıklama alanı
+        public string Department_Name { get; set; }
+    }
+}
