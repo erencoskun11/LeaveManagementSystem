525c6ddfa4e5f41d9716903a3bd4dd1e42bb3c50
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2026-02-13 17:50:08 +0300

    created

---
+++ Domain/Entities/Department.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Department : BaseEntity
+    {
+        public string Name { get; set; } // Örn: Yazılım, İK, Muhasebe
+        public string Description { get; set; }
+
+        public ICollection<Employee> Employees { get; set; }
+    }
+}
