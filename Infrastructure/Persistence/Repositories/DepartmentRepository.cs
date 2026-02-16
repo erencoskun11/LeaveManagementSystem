f7b1f633ed9f940818105324ed7343fbcc540756
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2026-02-16 17:27:25 +0300

    feat(persistence): add DepartmentRepository with HasEmployeesAsync check

---
+++ Infrastructure/Persistence/Repositories/DepartmentRepository.cs
@@ -0,0 +1,22 @@
+﻿using Application.Common.Interfaces.Repositories;
+using Domain.Entities;
+using Infrastructure.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence.Repositories
+{
+    public class DepartmentRepository : GenericRepository<Department>, IDepartmentRepository
+    {
+        public DepartmentRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+      
+
+        public async Task<bool> HasEmployeesAsync(int departmentId)
+        {
+            return await _context.Employees.AsNoTracking().AnyAsync(x => x.DepartmentId == departmentId && !x.IsDeleted);
+        }
+    }
+}
