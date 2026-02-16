169059331add30123392b61f916c89764b0cf054
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2026-02-16 17:24:35 +0300

    feat(persistence): add EF Core configuration for LeaveRequest entity

---
+++ Infrastructure/Persistence/Configurations/LeaveRequestConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    public class LeaveRequestConfiguration : IEntityTypeConfiguration<LeaveRequest>
+    {
+        public void Configure(EntityTypeBuilder<LeaveRequest> builder)
+        {
+            builder.HasOne(lr=>lr.Employee)
+                .WithMany(e=>e.LeaveRequests)
+                .HasForeignKey(lr=>lr.EmployeeId)
+                .OnDelete(DeleteBehavior.Restrict);
+            
+            builder.Property(lr=>lr.RequestComments)
+                .HasMaxLength(400)
+            .IsRequired(false);
+
+
+
+
+
+        }
+    }
+}
