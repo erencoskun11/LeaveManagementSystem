7dd3c7fc1060d942d352f074dd0979d6465d1894
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2026-02-17 17:33:38 +0300

    created

---
+++ Infrastructure/Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,76 @@
+﻿using Application.Common.Interfaces.Repositories;
+using Domain.Common; // BaseEntity için gerekli namespace
+using Infrastructure.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence.Repositories
+{
+    // DÜZELTME: Burada da 'where T : BaseEntity' olmalı
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        protected readonly ApplicationDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null, bool trackChanges = true)
+        {
+            IQueryable<T> query = _dbSet;
+
+            if (!trackChanges)
+                query = query.AsNoTracking();
+
+            if (filter != null)
+                query = query.Where(filter);
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<T> GetAsync(Expression<Func<T, bool>> filter, bool trackChanges = true)
+        {
+            IQueryable<T> query = _dbSet;
+
+            if (!trackChanges)
+                query = query.AsNoTracking();
+
+            return await query.FirstOrDefaultAsync(filter);
+        }
+
+        public async Task<bool> ExistsAsync(Expression<Func<T, bool>> filter)
+        {
+            return await _dbSet.AnyAsync(filter);
+        }
+    }
+}
